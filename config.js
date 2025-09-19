30a30774e7740629e8353c20193e18ca719ea772
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2025-09-19 11:53:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -30,15 +30,15 @@ yt: "https://www.youtube.com/@elrebelde.21",
 tiktok: "https://www.tiktok.com/@elrebelde.21",
 md: "https://github.com/elrebelde21/LoliBot-MD",
 fb: "https://www.facebook.com/elrebelde21",
-ig: "https://whatsapp.com/channel/0029VaahTuq1yT2H09LVe60W",
-nn: "https://whatsapp.com/channel/0029VaahTuq1yT2H09LVe60W", //Grupo ofc1
-nn2: "https://whatsapp.com/channel/0029VaahTuq1yT2H09LVe60W", //Grupo ofc2
-nn3: "https://whatsapp.com/channel/0029VaahTuq1yT2H09LVe60W", //Colab Loli & Gata
-nn4: "https://whatsapp.com/channel/0029VaahTuq1yT2H09LVe60W", //Enlace LoliBot
-nn5: "https://whatsapp.com/channel/0029VaahTuq1yT2H09LVe60W", //A.T.M.M
-nn6: "https://whatsapp.com/channel/0029VaahTuq1yT2H09LVe60W", //Dev support 
-nna: "https://whatsapp.com/channel/0029VaahTuq1yT2H09LVe60W",
-nna2: "https://whatsapp.com/channel/0029VaahTuq1yT2H09LVe60W"
+ig: "https://www.instagram.com/its_china_official",
+nn: "https://chat.whatsapp.com/HNDVUxHphPzG3cJHIwCaX5", //Grupo ofc1
+nn2: "https://chat.whatsapp.com/KDBt6S54riRCIpSZspkxhg", //Grupo ofc2
+nn3: "https://chat.whatsapp.com/GXNXKWSEckU1j1c7sItTxK", //Colab Loli & Gata
+nn4: "https://chat.whatsapp.com/Em4Byf4w5VgHObPvZQlfnM", //Enlace LoliBot
+nn5: "https://chat.whatsapp.com/J5hQvECqR4W6uTT84uiSBx", //A.T.M.M
+nn6: "https://chat.whatsapp.com/ILAHJeZsHh973tQ96i2aqS", //Dev support 
+nna: "https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A",
+nna2: "https://whatsapp.com/channel/0029Vah0NnV6mYPDQI7bpt0z"
 }
 
 //----------------------------------------------------
